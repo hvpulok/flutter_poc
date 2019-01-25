075f7059afafc31eaa6447c079fb2ccca40fc3b9
hvpulok <hvpulok@gmail.com>
2019-01-25 00:34:23 -0700

    Add sticky header example

--- lib/home-page/tabs/settings-tab/settings-tab.dart
+++ lib/home-page/tabs/settings-tab/settings-tab.dart
@@ -1,15 +1,7 @@
 import 'package:flutter/material.dart';
+import './sticky-header-demo.dart';
 
 class SettingsTab extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => new Container(
-        child: new Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            new Icon(Icons.settings, size: 150.0, color: Colors.black12),
-            new Text('Settings tab content')
-          ],
-        ),
-      );
+  Widget build(BuildContext context) => StickyHeaderDemo();
 }
